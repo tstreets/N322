8a0e94a96b2b0d40d84e2853d8bf691cd7607983
Ty Streets <tstreets@iu.edu>
2020-11-18 04:28:36 -0500

    not much

--- SimpleShifts/src/app/week-schedule/week-schedule.page.ts
+++ SimpleShifts/src/app/week-schedule/week-schedule.page.ts
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./week-schedule.page.scss'],
 })
 export class WeekSchedulePage implements OnInit {
+  public weekSchedule: any = {
+    days: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    hours: [],
+  }
 
-  constructor() { }
+  constructor() {
+    for(let h = 0; h < 24; h++) {
+      // change m < #; 1 yeilds hour mark; 2 yields half-hour mark;
+      for(let m = 0; m < 1; m++) {
+        this.weekSchedule.hours.push(`${h}:${(!!m) ? 3 : 0 }0`);
+      }
+    }
+  }
 
   ngOnInit() {
   }
