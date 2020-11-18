aeacf312b6bdbd08bcccf025d7b00b5ed2f1dcef
Ty Streets <tstreets@iu.edu>
2020-11-18 15:34:18 -0500

    environment

--- FirebaseApp/src/environments/environment.prod.ts
+++ FirebaseApp/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCNkafPEewU9DtuXvpwV9bV0y0NDOhkxyU",
+    authDomain: "fb-practive-sty.firebaseapp.com",
+    databaseURL: "https://fb-practive-sty.firebaseio.com",
+    projectId: "fb-practive-sty",
+    storageBucket: "fb-practive-sty.appspot.com",
+    messagingSenderId: "75397076489",
+    appId: "1:75397076489:web:9a8384a083223a90ad8ad8",
+    measurementId: "G-YJFR65HCVG"
+  }
 };
--- FirebaseApp/src/environments/environment.ts
+++ FirebaseApp/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCNkafPEewU9DtuXvpwV9bV0y0NDOhkxyU",
+    authDomain: "fb-practive-sty.firebaseapp.com",
+    databaseURL: "https://fb-practive-sty.firebaseio.com",
+    projectId: "fb-practive-sty",
+    storageBucket: "fb-practive-sty.appspot.com",
+    messagingSenderId: "75397076489",
+    appId: "1:75397076489:web:9a8384a083223a90ad8ad8",
+    measurementId: "G-YJFR65HCVG"
+  }
 };
 
 /*
